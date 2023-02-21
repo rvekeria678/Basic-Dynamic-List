0f9cfa1ef0ced8cddf31fd2d4388883f0856c1e1
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-02-21 11:30:12 -0500

    Implemented added/removing feature

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const add_btn = document.querySelector('#btn--add');
+const remove_btn = document.querySelector('#btn--remove');
+const list_wrap = document.querySelector("#list--wrapper");
+const text_input = document.querySelector("#text--input");
+var id = 0;
+
+add_btn.addEventListener('click', (event) => {
+    list_wrap.innerHTML +=
+        `<div class="bg-orange-500 h-[200px] border border-black p-4">
+            <h1 class="text-blue-300">`+ text_input.value + `</h1>    
+            <p font-md>Item can be found somewhere is this world</p>
+            <button onclick="deleteHandler(this)" class="mt-10 border rounded-xl bg-white md:hover:bg-gray-300 md:duration:200 px-2" id="del-`+ id + `">Delete</button>
+        </div>`;
+
+
+    ++id;
+    document.querySelector('#text--input').select();
+});
+
+remove_btn.addEventListener('click', (event) => {
+    alert('Im removing');
+});
+
+function deleteHandler(e) {
+    console.log(e.parentElement.remove());
+}
