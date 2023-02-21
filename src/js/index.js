f4f1015cfe3351eadcdd04bd7b50af52e3f5d3a7
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-02-21 14:46:02 -0500

    replaced innerHTML

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,23 @@ const text_input = document.querySelector("#text--input");
 var id = 0;
 
 add_btn.addEventListener('click', (event) => {
-    list_wrap.innerHTML +=
-        `<div class="bg-orange-200 h-[200px] border border-black p-4">
-            <h1 class="text-blue-600 font-bold">`+ text_input.value + `</h1>    
-            <p font-md>Item can be found somewhere is this world</p>
-            <button onclick="deleteHandler(this)" class="mt-10 border rounded-lg bg-white md:hover:bg-gray-300 md:duration:200 px-2" id="del-`+ id + `">Delete</button>
-        </div>`;
-    console.log(list_wrap);
+    const card_container = document.createElement('div');
+    const card_title = document.createElement('h1');
+    const card_text = document.createElement('p');
+    const card_del = document.createElement('button');
 
-    ++id;
+    card_title.innerText = (text_input !== null) ? text_input.value : "Temp Title";
+    card_text.innerText = "Some random text to get things going";
+    card_del.innerText = "Delete";
+
+    card_container.append(card_title);
+    card_container.append(card_text);
+    card_container.append(card_del);
+
+    list_wrap.append(card_container);
+
+    card_container.setAttribute('class', 'bg-orange-200 text-center');
+
     document.querySelector('#text--input').select();
 });
 
