6c9d7ec5b7a91d51513384ce2ee4fc26faeab257
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-02-21 11:36:25 -0500

    Fixed id bug

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,12 @@ var id = 0;
 
 add_btn.addEventListener('click', (event) => {
     list_wrap.innerHTML +=
-        `<div class="bg-orange-500 h-[200px] border border-black p-4">
-            <h1 class="text-blue-300">`+ text_input.value + `</h1>    
+        `<div class="bg-orange-200 h-[200px] border border-black p-4">
+            <h1 class="text-blue-600 font-bold">`+ text_input.value + `</h1>    
             <p font-md>Item can be found somewhere is this world</p>
-            <button onclick="deleteHandler(this)" class="mt-10 border rounded-xl bg-white md:hover:bg-gray-300 md:duration:200 px-2" id="del-`+ id + `">Delete</button>
+            <button onclick="deleteHandler(this)" class="mt-10 border rounded-lg bg-white md:hover:bg-gray-300 md:duration:200 px-2" id="del-`+ id + `">Delete</button>
         </div>`;
-
+    console.log(list_wrap);
 
     ++id;
     document.querySelector('#text--input').select();
